e4aa62988bebc943651306ad7aea05ffad7bfc3d
mrpauloen <69369238+mrpauloen@users.noreply.github.com>
2020-11-15 22:26:27 +0100

    update

--- comments.php
+++ comments.php
@@ -32,11 +32,7 @@ if ( post_password_required() )
     <div class="form-check pl-0">
       <label title="' . esc_attr( 'Your data will be stored in this browser and added automaticly so next time you don\'t need to put it again.', 'the-bootstrap-blog' ) . '" for="wp-comment-cookies-consent">
       <input class="" type="checkbox" id="wp-comment-cookies-consent" name="wp-comment-cookies-consent" value="yes">
-<small>Remember me</small> ' . ( ( ! wp_is_mobile() ) ? '<svg width="0.8em" height="0.8em" viewBox="0 0 16 16" class="" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-  <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-  <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588z"/>
-  <circle cx="8" cy="4.5" r="1"/>
-</svg>' : '***' ) . '
+<small>Remember me</small> ' . the_bootstrap_blog__get_icon_svg( 'info-square-fill', '16' ) . '
       </label>
     </div>
   </div>',
@@ -124,7 +120,6 @@ if ( $have_comments ) {
 	wp_list_comments( array(
     'avatar_size' => get_theme_mod( 'avatar_size', 45 ),
     'type'  => 'all',
-    'reply_text'  => __( '&rarr; Reply', 'the-bootstrap-blog' ),
     'walker' 		=> new The_Bootstrap_Blog_Comments_Walker(),
 	) );
 
